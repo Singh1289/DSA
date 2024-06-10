bcb3dd617a878a0342a7825b60d061d324f3237a
Group030 <Group030@infoway.com>
2024-06-10 16:17:43 +0530

    day_1 commit 2

---
+++ Stack/src/day_1/EmployeeStack.java
@@ -0,0 +1,56 @@
+package day_1;
+
+public class EmployeeStack {
+	private int top;
+	private Employee arr[];
+	
+	public EmployeeStack(int size) {
+		top = -1;
+		arr = new Employee[size] ;
+	}
+	public EmployeeStack() {
+		top = -1;
+		arr = new Employee[10];
+	}
+	
+	// is Empty  isFull pop push peek
+	public boolean isEmpty() {
+		return (top==-1)? true: false;
+	}
+	public boolean isFull() {
+		return (top==arr.length-1)? true: false;
+	}
+	
+	public void push(Employee a) {
+		if(isFull()) {
+			System.out.println("\n Stack overflow...");
+			return;
+		}else {
+			top++;
+			arr[top]=a;
+			return;
+		}
+	}
+	
+	public Employee pop() {
+		Employee d=new Employee();        
+		if(isEmpty()) {
+			return d;
+		}else {
+			d=arr[top];
+			top--;
+			return d;
+		}
+	}
+	
+	public Employee peek() {
+		Employee d=new Employee();
+		if(isEmpty()) {
+			return d;
+		}else {
+			d= arr[top];
+			return d;
+		}
+	}
+	
+}
---
+++ Stack/src/day_1/IntStack2c.java
@@ -0,0 +1,91 @@
+package day_1;
+
+public class IntStack2c {
+	private int topA ,topB;
+	private int []arr;
+	public IntStack2c() {
+		topA =-1;
+		topB=arr.length;
+		arr= new int[10];
+	}
+	public IntStack2c(int topB) {
+		topA =-1;
+		arr= new int[topB];
+		this.topB = topB;
+	}
+	 
+	public boolean isFull() {
+		return((topB-topA)==1) ? true:false;
+	}
+	public boolean isEmptyA() {
+		return((topA)==-1) ? true:false;
+	}
+	public boolean isEmptyB() {
+		return((topB)==arr.length) ? true:false;
+	}
+	
+	public void pushA(int a) {
+	
+		if(isFull()) {
+			System.out.println("Stack Overflow.....");
+			return ;
+		}else {
+			topA++;
+			arr[topA]=a;
+			return;
+		}
+	}
+	public void pushB(int b) {
+		
+		if(isFull()) {
+			System.out.println("Stack Overflow.....");
+			return ;
+		}else {
+			topB--;
+			arr[topB]=b;
+			return;
+		}
+	}
+	public int popA() {
+		int d=-999;
+		if(isEmptyA()) {
+			return d;
+		}else {
+			d= arr[topA];
+			topA--;
+			
+			
+			return d;
+		}
+	}
+	public int popB() {
+		int d=-999;
+		if(isEmptyB()) {
+			return d;
+		}else {
+			
+			topB++;
+			
+			
+			return d;
+		}
+	}
+	public int peekA() {
+		int d=-999;
+		if(isEmptyA()) {
+			return d;
+		}else {
+			d= arr[topA];
+			return d;
+		}
+	}
+	public int peekB() {
+		int d=-999;
+		if(isEmptyB()) {
+			return d;
+		}else {
+			d= arr[topB];
+			return d;
+		}
+	}
+}
