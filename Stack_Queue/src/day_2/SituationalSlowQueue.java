e54780b3e60037b9cb7361755abf160ec771d50f
Singh1289 <singh.1289abhi@gmail.com>
2024-06-11 17:58:27 +0530

    day_2 LinkedList added

--- Stack_Queue/src/day_2/SituationalSlowQueue.java
+++ Stack_Queue/src/day_2/SituationalSlowQueue.java
@@ -1,69 +1,65 @@
 package day_2;
 
 public class SituationalSlowQueue {
-	   private int front ,rear;
-	   private int []arr;
+	private int front,rear;
+	private int arr[];
+	public SituationalSlowQueue() {
+		front=rear=-1;
+		arr= new int [10];
+	}
+	public SituationalSlowQueue(int a) {
+		front=rear=-1;
+		arr= new int [a];
+	}
+	public boolean isFull() {
+		return (rear==arr.length-1)? true:false;
+	}
+	public boolean isEmpty() {
+		return ((front== -1)||(front==rear+1))? true:false;
+	}
+	
+	public void insert(int a) {
+		if (isFull()) {
+			if(front==0) {
+			System.out.println("Queue is full..!!");
+			return;
+			}else {
+				shiftArray();
+			}			
+		}
+		if(front==-1)front++;
+		
+		rear++;
+		arr[rear]=a;
+		return;
+	}
+	
+	private void shiftArray() {
+		int i,j,d=front;
+		for(i=d;i>=0;i--) {
+			for(j)
+		}
+		
+	}
+	public int remove() {
+		int d = -999;  //arbitrary constant
+		if(isEmpty()) {
+			System.out.println("Queue is Empty...!!!");
+			return d;
+		}
 
-	   public SituationalSlowQueue() {
-	    front = rear = -1;
-	    arr=new int[10];
-	   }
-
-	   public SituationalSlowQueue(int n) {
-	      front=rear=-1;
-	      arr = new int[n];
-	   }
-
-	   public boolean isEmpty() {
-	      return (front==-1)?true:false;
-	   }
-	   public boolean isFull() {
-	      return (((front==rear+1 )&&(front==0))||rear==arr.length-1)?true:false;
-	   }
-	   public void insert(int x) {
-	      if(isFull()) {
-	         System.out.println("Queue is full");
-	         return;
-	      }
-	      if  (front==-1) {
-	         front=0;
-	      }
-	      if(rear==arr.length-1) {
-	         rear=0;
-	      }
-	      else
-	         rear++;
-	      
-	      arr[rear]=x;
-	      return;
-	   }
-	   public int remove() {
-	      int d = -999;
-	      if(isEmpty()) {
-	      //   System.out.println("Queue is empty");
-	         return d;
-	      }
-	      d=arr[front];
-	      if(front==rear) {
-	         front=-1;
-	         rear=-1;
-	         } else if (front ==arr.length-1) {
-	         front=0;
-	      }else{
-	         front++;
-	      }
-	      return d;
-	   }
-	   
-	    public int peek(){
-	       int d = -999;
-	       if(isEmpty()) {
-	          System.out.println("Queue is empty");
-	          return d;
-	       }
-	       d=arr[front];
-	       return d;
-	   }
-
+		d= arr[front++];
+		return d;		
+	}
+	
+	public int peek() {
+		int d = -999;  //arbitrary constant
+		if(isEmpty()) {
+			System.out.println("Queue is Empty...!!!");
+			return d;
+		}
+		d = arr[front];
+		return d;	
+	}
 	}
 
