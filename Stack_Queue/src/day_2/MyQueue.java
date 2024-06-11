e7604ceed224761a0cf91505aa7745c64c65e1ef
Singh1289 <singh.1289abhi@gmail.com>
2024-06-11 11:37:25 +0530

    lab Day_2 2nd

---
+++ Stack_Queue/src/day_2/MyQueue.java
@@ -0,0 +1,55 @@
+package day_2;
+
+public class MyQueue {
+	private int front,rear;
+	private int arr[];
+	public MyQueue() {
+		front=rear=-1;
+		arr= new int [10];
+	}
+	public MyQueue(int a) {
+		front=rear=-1;
+		arr= new int [a];
+	}
+	public boolean isFull() {
+		return (rear==arr.length-1)? true:false;
+	}
+	public boolean isEmpty() {
+		return ((front== -1)||(front==rear+1))? true:false;
+	}
+	
+	public void insert(int a) {
+		if (isFull()) {
+			System.out.println("Queue is full..!!");
+			return;
+		}
+		if(front==-1)front++;
+		
+		rear++;
+		arr[rear]=a;
+		return;
+	}
+	
+	public int remove() {
+		int d = -999;  //arbitrary constant
+		if(isEmpty()) {
+			System.out.println("Queue is Empty...!!!");
+			return d;
+		}
+	//	d = arr[front];
+	//	front++;
+		d= arr[front++];
+		return d;		
+	}
+	
+	public int peek() {
+		int d = -999;  //arbitrary constant
+		if(isEmpty()) {
+			System.out.println("Queue is Empty...!!!");
+			return d;
+		}
+		d = arr[front];
+		return d;	
+	}
+	
+}
